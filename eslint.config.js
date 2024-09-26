31efb0352c4aa641ce3213c8e7b9376d107a0167
cesar <cesar@MacBook-Pro-de-Cesar.local>
2024-09-25 22:06:35 -0300

    Migrate ESLint configuration to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+import { Linter } from 'eslint';
+
+/** @type {Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    // Tus reglas personalizadas
+  },
+};
+
+export default config;
