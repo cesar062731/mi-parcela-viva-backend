18d75fe12a927ef18535e98afab9c42f2b440c96
cesar <cesar@MacBook-Pro-de-Cesar.local>
2024-09-25 22:15:54 -0300

    Fix ESLint configuration to use .mjs extension

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// eslint.config.cjs
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    // Tus reglas personalizadas
+  },
+};
+
+module.exports = config;
