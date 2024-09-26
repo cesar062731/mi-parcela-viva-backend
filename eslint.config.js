d2ee49e3e87d2e4e149f4cd07a0ef08775d3f5ac
cesar <cesar@MacBook-Pro-de-Cesar.local>
2024-09-25 22:23:08 -0300

    Update ESLint configuration to use flat config system

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,53 @@
 // eslint.config.js
-import { Linter } from 'eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
 
-/** @type {Linter.Config} */
-const config = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    // Tus reglas personalizadas
-  },
-};
+const compat = new FlatCompat({
+  baseDirectory: import.meta.url,
+  resolvePluginsRelativeTo: import.meta.url,
+});
 
-export default config;
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      // Tus reglas personalizadas
+    },
+  },
+  ...compat.config({
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      '@typescript-eslint',
+    ],
+    rules: {
+      // Tus reglas personalizadas
+    },
+  }),
+];
